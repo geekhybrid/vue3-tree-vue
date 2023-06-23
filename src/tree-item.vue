dc9a62d895d1991704d89246f4ac8ed23f14fd76
geekhybrid <hocahaenyi17@gmail.com>
2023-06-23 08:23:17 +0100

    Support Customisable expander

--- src/tree-item.vue
+++ src/tree-item.vue
@@ -1,36 +1,34 @@
 <template>
-    <div class="d-flex align-items-center" @contextmenu.prevent="$emit('onContextMenu', { item, event: $event })">
-            <div class="horizontal-dashes" v-if="$attrs.isNested" />
-            <span class="chevron-right" ref="chevron"
-                :class="{'hide-chevron': !item.children || item.children.length < 1}"
-                @click="toggleExpand()">
-            </span>
+  <div class="d-flex align-items-center" @contextmenu.prevent="$emit('onContextMenu', { item, event: $event })">
+    <div class="horizontal-dashes" v-if="$attrs.parent" />
+    <!-- {{ $attrs }} -->
 
-            <div class="pointer" :class="{'selected-tree-item': !isCheckable && isSelected}" style="width: 100%">
-                <div v-if="!isRenaming" @dblclick="beginRenaming">
-                    <div v-if="isCheckable" style="display: flex">
-                        <input @contextmenu.prevent @change="updateCheckState" type="checkbox" ref="checkbox" />
-                        <div class="d-flex" style="margin-left: 0.4em; margin-right: 0.4em">
-                            <slot name="icon"></slot>
-                            <slot name="prepend"></slot>
-                        </div>
-                        <label for="checkbox" v-if="!isRenaming">{{ item.name }}</label>
-                        <!-- <input v-model="item.name" v-else /> -->
-                    </div>
-                    <div class="d-flex" v-else @click="treeState?.emitItemSelected(item)">
-                        <slot name="icon"></slot>
-                        <slot name="prepend"></slot> 
-                        <span>{{ item.name }}</span>
-                    </div>
-                </div>
-                <input
-                    ref="rename-box"
-                    v-model="item.name" v-else
-                    v-on:keyup.enter="finishRenaming"
-                    @blur="finishRenaming"
-                />
-            </div>
+    <div @click="toggleExpand()" v-show="item.children && item.children.length > 0">
+      <slot name="expander">
+        <span class="chevron-right" ref="chevron"></span>
+      </slot>
+    </div>
+
+    <div class="pointer tree-item" :class="{ 'selected-tree-item': !isCheckable && isSelected }" style="width: 100%">
+      <div v-if="!isRenaming" @dblclick="beginRenaming">
+        <div v-if="isCheckable" style="display: flex">
+          <input @contextmenu.prevent @change="updateCheckState" type="checkbox" ref="checkbox" />
+          <div class="d-flex">
+            <slot name="icon"></slot>
+            <slot name="prepend"></slot>
+          </div>
+          <label for="checkbox" v-if="!isRenaming">{{ item.name }}</label>
+          <!-- <input v-model="item.name" v-else /> -->
         </div>
+        <div class="d-flex" v-else @click="treeState?.emitItemSelected(item)">
+          <slot name="icon"></slot>
+          <slot name="prepend"></slot>
+          <span>{{ item.name }}</span>
+        </div>
+      </div>
+      <input ref="rename-box" v-model="item.name" v-else v-on:keyup.enter="finishRenaming" @blur="finishRenaming" />
+    </div>
+  </div>
 </template>
 
 <script src="./tree-item.ts" lang="ts" />
